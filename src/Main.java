6be75cae37536f5d7fa2a60e8ca041cc73c077d3
Your Name <you@example.com>
2024-12-05 01:51:47 +0300

    0412

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,85 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main{
+//13.02
     public static void main(String[] args) {
 
-        Student student1 = new Student("pasha", 20);
-        Student student2 = new Student("masha", 19);
-        Student student3 = new Student("sasha", 18);
+        Student student1 = new Student("pasha1", 20, 2, "M", 9.3);
+        Student student2 = new Student("pasha2", 21, 3, "G", 4.3);
+        Student student3 = new Student("pasha3", 22, 4, "G", 2.3);
+        Student student4 = new Student("pasha4", 23, 5, "M", 6.3);
+        Student student5 = new Student("pasha5", 24, 6, "G", 7.3);
+        Student student6 = new Student("pasha6", 25, 7, "M", 8.3);
+        Student student7 = new Student("pasha7", 26, 8, "G", 2.3);
+
 
         Student.arrStudents.add(student1);
         Student.arrStudents.add(student2);
         Student.arrStudents.add(student3);
+        Student.arrStudents.add(student4);
+        Student.arrStudents.add(student5);
+        Student.arrStudents.add(student6);
+        Student.arrStudents.add(student7);
 
-        StudentsCheck studentsCheck = new StudentsCheck();
+        Main main = new Main();
 
-        studentsCheck.info(Student.arrStudents, (Student e) -> {return e.getAge()>18;});
+        main.testStudent(Student.arrStudents, new CheckOverGrade());
+
+//        main.printStudentOverGrade(Student.arrStudents);
+//        System.out.println("----------------------------------------");
+//        main.printStudentUnderAge(Student.arrStudents);
+//        System.out.println("----------------------------------------");
+//        main.printStudentMixCondition(Student.arrStudents);
+//        System.out.println("----------------------------------------");
 
     }
 
+
+
+    public void testStudent(ArrayList<Student> al, StudentCheck sc){
+        for (Student s : al){
+            if(sc.check(s)){
+                System.out.println(s);
+            }
+        }
+    }
+
+//    public void printStudentOverGrade(ArrayList<Student> t){
+//        for (Student s : t){
+//            if(s.getAvgGrade() > 5) {
+//                System.out.println(s);
+//            }
+//        }
+//    }
+//
+//    public void printStudentUnderAge(ArrayList<Student> t){
+//        for (Student s : t){
+//            System.out.println(s);
+//        }
+//    }
+//
+//    public void printStudentMixCondition(ArrayList<Student> t){
+//        for (Student s : t){
+//            System.out.println(s);
+//        }
+//    }
+
+}
+
+//-----------------------------------------------------------------------------------------
+class Test{
+
+}
+
+class CheckOverGrade implements StudentCheck{
+    @Override
+    public boolean check(Student s) {
+        return s.getAvgGrade() > 5;
+    }
+}
+
+interface StudentCheck{
+    boolean check(Student s);
 }
