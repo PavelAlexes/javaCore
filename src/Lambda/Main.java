ebbd7557de39bc28ca48aae25d428ef3f805e960
Your Name <you@example.com>
2025-03-17 16:40:12 +0300

    17march2025

--- src/Lambda/Main.java
+++ src/Lambda/Main.java
@@ -1,4 +1,4 @@
-//package Lambda.Test;
+package Lambda;//package Lambda.Test;
 //
 //import Lambda.GetList;
 //
@@ -34,3 +34,41 @@
 //
 //    }
 //}
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("aaaaa");
+        list.add("aaaa");
+        list.add("aaa");
+
+        list.sort((o1, o2) -> {
+            if (o1.length() > o2.length()){
+                return 1;
+            } else if (o2.length() > o1.length()) {
+                return -1;
+            }else return 0;
+        });
+
+
+//        list.sort(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                if (o1.length() > o2.length()){
+//                    return 1;
+//                } else if (o2.length() > o1.length()) {
+//                    return -1;
+//                }else return 0;
+//            }
+//        });
+        System.out.println(list);
+    }
+        }
+
